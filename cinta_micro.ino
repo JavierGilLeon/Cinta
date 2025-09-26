82858cc24bdbc2fef12d5a123a9daab54c37d1ca
Jaime Garrido González <jaimegarridogonz04@gmail.com>
2025-09-26 12:05:42 +0200

    Add files via upload
    Actualización código

--- cinta_micro.ino
+++ cinta_micro.ino
@@ -1,55 +1,107 @@
 #define sensor 13
 
-#define in1 12
-#define in2 14
+#define cintaIn1 12
+#define cintaIn2 14
 
-typedef enum{Stop = 0,
-             Run  = 1
-}Estados;
+#define bombaIn1 25
+#define bombaIn2 26
+
+typedef enum { Stop = 0,
+               Run = 1,
+               Fill = 2
+} Estados;
 
 Estados cinta = Run;
 hw_timer_t *timer = NULL;
 
 
+int Started = 0;
 
-void IRAM_ATTR timerInterrupt() {
 
+void ARDUINO_ISR_ATTR timerIsr() {
+  Serial.println("Interrupt");
+
+  digitalWrite(bombaIn1, LOW);
+
+  cinta = Stop;
 }
 
 
-void setup()
-{
-  pinMode(sensor, INPUT); //
-  pinMode(in1, OUTPUT); //
-  pinMode(in2, OUTPUT); //
+void MoveCinta(float vel){
+
+  if(vel < 0 || vel > 2.2) return;
+
+  vel = map(vel,0,2.2,0,255);
+
+  analogWrite(cintaIn1, vel);
+  analogWrite(cintaIn2, 0);
+}
+
+void LlenaBote(int ml){
+  long int t;
+  t = map(ml,0,32,0,30000000);
+
+  if(!Started){
+  digitalWrite(bombaIn1,HIGH);
+  digitalWrite(bombaIn2,LOW);
+
+  timerAlarm(timer, t, true, 0);
+  timerStart(timer);
+  Started = 1;
+  }
+}
+
+
+void setup() {
+  pinMode(sensor, INPUT);     //
+  pinMode(cintaIn1, OUTPUT);  //
+  pinMode(cintaIn2, OUTPUT);  //
+
+  pinMode(bombaIn1, OUTPUT);  //
+  pinMode(bombaIn2, OUTPUT);  //
+
   Serial.begin(115200);
 
-  timer = timerBegin(timer,)
+  timer = timerBegin(1000000);
+  if (timer == NULL) Serial.println("Timer no iniciado");
+
+  timerAttachInterrupt(timer, &timerIsr);
+
+
+  //digitalWrite(bombaIn1, HIGH);*/
 }
-void loop()
-{
 
- int lectura = digitalRead(sensor);
+void loop() {
+    float vel = 2;
+    float ml = 10;
 
-  switch(lectura){
-    case Stop:
-      digitalWrite(in1,HIGH);
-      digitalWrite(in2,LOW);
-    break;
+    switch(cinta){
+      case Run:
+        if(digitalRead(sensor)) cinta = Fill;
+        MoveCinta(vel);    
+        break;
+
+      case Fill:
+        MoveCinta(0);
+        LlenaBote(ml);
+        break;
+      
+      case Stop: break;
+    }
+  
+   /*Serial.print("t: ");
+
+   Serial.println(timerRead(timer));*/
+
+  int lectura = digitalRead(sensor);
+  
+
+
+  /*switch(lectura){
     
-    case Run:
-      digitalWrite(in2,HIGH);
-      digitalWrite(in1,LOW);
-    break;
-
-    default: 
-      lectura = Run; 
   }
 
   Serial.println(lectura);
-
-  delay(100);
+*/
+ // delay(100);
 }
-
-
-
